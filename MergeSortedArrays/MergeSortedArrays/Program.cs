4e0ab5b27a21114d33900b749bb1c600c0b5a88f
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-03-14 07:15:02 -0500

    Merge sort in Csharp with rider eap.

---
+++ MergeSortedArrays/MergeSortedArrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MergeSortedArrays
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] myArray = { 3, 4, 6, 10, 11, 15 };
+            int[] alicesArray = { 1, 5, 8, 12, 14, 19 };
+
+            // merge sort the arrays while we add them..
+            PrintArray(Mergesorts(myArray, alicesArray));
+        }
+
+        public static int[] Mergesorts(int[] arrie1, int[] arrie2)
+        {
+
+            var arrie3 = arrie1.Concat(arrie2).ToArray();
+            return arrie3;
+        }
+
+        public static void PrintArray(int[] arrg)
+        {
+            foreach (var n in arrg)
+            {
+                Console.WriteLine(Convert.ToString(n));
+            }
+        }
+    }
+}
---
+++ MergeSortedArrays/MergeSortedArrays/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("MergeSortedArrays")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MergeSortedArrays")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("ADA52846-ED0A-4EA5-A0BA-53E6DB23A7C9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
