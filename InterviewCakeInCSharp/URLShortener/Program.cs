ccca9f0dfe0444d94ddff4fe5c16cfea8da0c6b3
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-03-15 09:26:48 -0500

    new solution for int cake c sharp problems.

---
+++ InterviewCakeInCSharp/URLShortener/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace URLShortener
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Customize your Shortened Address? ");
+            var custom = Console.Read();
+            var url = Console.ReadLine();
+
+            if (custom.ToString().ToLower() != "yes")
+            {
+
+                url = generateShortURL(url);
+            }
+            else
+            {
+                url = generateCustomURL(url);
+            }
+
+            Console.WriteLine(url);
+        }
+
+        public static string generateShortURL(string url)
+        {
+            return "URL";
+        }
+
+        public static string generateCustomURL(string url)
+        {
+            return "URL";
+        }
+
+        // modify or delete?
+
+        // secure?
+
+        // exists forever?
+
+        // custom shorter link is available
+
+        // analytics
+
+
+    }
+}
---
+++ MergeSortedArrays/Ca.ke/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ca.ke
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
