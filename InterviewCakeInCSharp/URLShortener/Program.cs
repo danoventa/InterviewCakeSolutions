be8fdea799e85c9983fcd7ef8ac2a548ce3df2d6
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-03-15 13:40:36 -0500

    There's an issue with the custom id. Quits unexpectantly.

--- InterviewCakeInCSharp/URLShortener/Program.cs
+++ InterviewCakeInCSharp/URLShortener/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Policy;
 
 namespace URLShortener
 {
@@ -8,31 +10,54 @@ namespace URLShortener
     {
         public static void Main(string[] args)
         {
-            Console.Write("Customize your Shortened Address? ");
-            var custom = Console.Read();
-            var url = Console.ReadLine();
+            var keepItUp = true;
+            var url = "";
 
-            if (custom.ToString().ToLower() != "yes")
+            var linkMap = new Dictionary<string, string>();
+            // Probs want this to be in a server like architecture, in order to store all links, and reference them somehow.
+
+            while (keepItUp)
             {
+                Console.Write("Customize your link? ");
+                var isCustom = Console.ReadLine();
+                Console.Write("Your Address? ");
+                url = Console.ReadLine();
 
-                url = generateShortURL(url);
-            }
-            else
-            {
-                url = generateCustomURL(url);
+                url = isCustom != null && isCustom.ToLower() != "yes" ? generateShortURL(url, linkMap) : generateCustomURL(url, linkMap);
+                Console.WriteLine("This is your new URL: " + url);
+                Console.Write("Another? ");
+
+                var another = Console.ReadLine();
+                keepItUp = another != null && another.ToLower() != "yes" ? false : true;
             }
 
-            Console.WriteLine(url);
+            printKeys(linkMap);
         }
 
-        public static string generateShortURL(string url)
+        public static void printKeys(Dictionary<String, String> linkMap)
         {
-            return "URL";
+            foreach(var kp in linkMap)
+            {
+                 Console.Write("\n " + kp.Key +  " has " + kp.Value);
+            }
         }
 
-        public static string generateCustomURL(string url)
+        public static string generateShortURL(string url, Dictionary<string, string> linkMap)
         {
-            return "URL";
+
+            var hash = url.GetHashCode();
+            var newUrl = "ca.ke/" + Convert.ToString(hash);
+            linkMap.Add(newUrl, url);
+            return newUrl;
+        }
+
+        public static string generateCustomURL(string url, Dictionary<string, string> linkMap)
+        {
+            Console.WriteLine("What would you like the custom name to be? ");
+            var customName = Console.Read();
+            var newUrl = "ca.ke/" + customName;
+            linkMap.Add(newUrl, url);
+            return newUrl;
         }
 
         // modify or delete?
