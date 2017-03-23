6ac5edf6739cedee4f0c99eba5cf7305f26c736c
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-03-23 18:25:41 -0500

    New problem, starting with given info.

---
+++ InterviewCakeInCSharp/BinarySearchTreeChecker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinarySearchTreeChecker
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+        }
+    }
+
+    internal class BinaryTreeNode
+    {
+        public
+    }
+}
