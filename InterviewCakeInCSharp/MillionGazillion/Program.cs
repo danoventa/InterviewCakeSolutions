b461970ad5075c27e4884624bf223a9c11443e39
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-03-23 13:10:35 -0500

    Implementing InterviewCake solution. Need testing, and traversing of the trie. Also memoizing by prefix.

---
+++ InterviewCakeInCSharp/MillionGazillion/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Net;
+using System.Threading;
+
+namespace MillionGazillion
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var trie = new Trie();
+            var runtTrie = true;
+            while (runtTrie)
+            {
+                runtTrie = WillAddNewAddress();
+                if (runtTrie)
+                {
+                    AddNewAddress(trie);
+                }
+            }
+        }
+
+        public static void AddNewAddress(Trie trie)
+        {
+            Console.WriteLine("What is the new address");
+            var newAddress = Console.ReadLine();
+
+            trie.ProcessCurrentNode(newAddress);
+        }
+
+        public static bool WillAddNewAddress()
+        {
+            Console.WriteLine("Would you like to add another site?");
+            var continueReading = Console.ReadLine()?.ToLower();
+
+
+            return continueReading == "y" || continueReading == "yes";
+        }
+    }
+
+    internal class Trie
+    {
+        private const char EndOfWord = '\0';
+
+        private readonly TrieNode _root = new TrieNode();
+
+        public bool ProcessCurrentNode(string word)
+        {
+            var currentNode = _root;
+            bool isWordNew = false;
+
+            foreach (var character in word)
+            {
+                if (!currentNode.NodeExists(character))
+                {
+                    isWordNew = true;
+                    currentNode.AppendNewNode(character);
+
+                }
+
+                currentNode = currentNode.GetNode(character);
+            }
+
+            if(!currentNode.NodeExists(EndOfWord))
+            {
+                isWordNew = true;
+                currentNode.AppendNewNode(EndOfWord);
+            }
+
+            return isWordNew;
+        }
+
+
+    }
+
+    internal class TrieNode
+    {
+        private Dictionary<char, TrieNode> _trieNodes = new Dictionary<char, TrieNode>();
+
+        public bool NodeExists(char character)
+        {
+            return _trieNodes.ContainsKey(character);
+        }
+
+        public void AppendNewNode(char character)
+        {
+            _trieNodes[character] = new TrieNode();
+        }
+
+        public TrieNode GetNode(char character)
+        {
+            return _trieNodes[character];
+        }
+    }
+}
---
+++ InterviewCakeInCSharp/nthFibonacci/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+namespace nthFibonacci
+{
+    internal class Program
+    {
+        private static double phi = (1 + Math.Sqrt(5)) / 2;
+        public static void Main(string[] args)
+        {
+            const int nth = 45;
+
+            // fib of 1 = 1
+            //
+
+            // nthFiboCalculated(nth); // precision is not important, use golden ratio
+            Console.WriteLine(Convert.ToString(nthFiboCalculated(nth))); // recursive probs want to ameliorate
+            // learn how do matrix multipliction to drop this down to log n
+            Console.WriteLine(Convert.ToString(nthFiboRecursive(nth))); // recursive probs want to ameliorate
+        }
+
+        private static int dictionaryCheck(int number, Dictionary<int, int> visited)
+        {
+            int value;
+            if (visited.TryGetValue(number, out value))
+            {
+                return value;
+            }
+            return number;
+        }
+
+        private static int nthFiboRecursiveHelper(int number, Dictionary<int, int> visited)
+        {
+            if (number == 0)
+            {
+                return 0;
+            }
+            if (number == 1)
+            {
+                return 1;
+            }
+            if (number == 5)
+            {
+                return 5;
+            }
+            var visNum = dictionaryCheck(number, visited);
+            if (visNum != number )
+            {
+                return visNum;
+            }
+
+            return nthFiboRecursiveHelper(number - 1, visited) + nthFiboRecursiveHelper(number - 2, visited);
+        }
+
+
+        public static int nthFiboRecursive(int number)
+        {
+            return nthFiboRecursiveHelper(number, new Dictionary<int, int>());
+        }
+
+        public static double nthFiboCalculated(int nth)
+        {
+            return Math.Round((Math.Pow(phi, nth) - Math.Pow(-phi, -nth)) / (2*phi - 1));
+        }
+    }
+}
