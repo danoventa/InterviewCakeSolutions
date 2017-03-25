95eace6af792564f04f852f0c4e679b8615406de
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-03-24 19:15:19 -0500

    Update .netcore prep

---
+++ DotNetCore1CS6/SpeakingCSh/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpeakingCSh
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
