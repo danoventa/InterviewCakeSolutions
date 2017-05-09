4543e1a082ea98d2352879d7e88c9fa6ed00f3b1
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-05-09 06:59:34 -0500

    CPP solutions folder.

---
+++ InterviewCakeInCPP/cmake-build-debug/CMakeFiles/InterviewCakeInCPP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/InterviewCakeInCPP.dir/main.cpp.o"
+  "InterviewCakeInCPP.pdb"
+  "InterviewCakeInCPP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/InterviewCakeInCPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
