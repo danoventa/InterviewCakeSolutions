a3393fcf52513402ccd09c7ee3f1e3bc7b22d919
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-05-10 23:36:44 -0500

    [WIP] HashSet is not in expected order.

--- InterviewCakeInCSharp/HiCal/Program.cs
+++ InterviewCakeInCSharp/HiCal/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
 using System.Runtime.Remoting.Messaging;
 
 namespace HiCal
@@ -9,25 +11,59 @@ namespace HiCal
         public static void Main(string[] args)
         {
             var meeting1 = new Meeting(2, 3);
-            var meeting2 = new Meeting(2, 3);
-            var meeting3 = new Meeting(2, 3);
-            var meeting4 = new Meeting(2, 3);
-            
+            var meeting2 = new Meeting(3, 4);
+            var meeting3 = new Meeting(6, 9);
+            var meeting4 = new Meeting(1, 4);
+
             var initialMeetings = new List<Meeting> {meeting1, meeting2, meeting3, meeting4};
 
             var mergedMeetings = MergeRange(initialMeetings);
             foreach (var meeting in mergedMeetings)
             {
-                Console.Write(Convert.ToString(meeting.StartTime) + " - " + Convert.ToString(meeting.EndTime));
+                Console.WriteLine(Convert.ToString(meeting.StartTime) + " - " + Convert.ToString(meeting.EndTime));
             }
         }
 
         public static List<Meeting> MergeRange(List<Meeting> meetings)
         {
-            Console.Write("THIS IS A TEST");
-            var meeting1 = new Meeting(1, 2);
-            var retList = new List<Meeting> {meeting1};
-            return retList;
+            var mergedTimes = new List<int>();
+            var mergedMeetings = new List<Meeting>();
+            var meetingSet = new HashSet<int>();
+
+            foreach (var meeting in meetings)
+            {
+                for (var i = meeting.StartTime; i <= meeting.EndTime; i++)
+                {
+                    meetingSet.Add(i);
+                }
+            }
+
+           
+
+            foreach (var time in meetingSet)
+            {
+                Console.WriteLine(Convert.ToString(time));
+                if (mergedTimes.Count > 0)
+                {
+                    if (mergedTimes.Last() + 1 == time)
+                    {
+                        mergedTimes.Add(time);
+                    }
+                    else
+                    {
+                        if (mergedTimes.Count > 0)
+                        {
+                            mergedMeetings.Add(new Meeting(mergedTimes.First(), mergedTimes.Last()));
+                        }
+                    }
+                }
+                else
+                {
+                    mergedTimes.Add(time);
+                }
+            }
+
+            return mergedMeetings;
         }
     }
 
