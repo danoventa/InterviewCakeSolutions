783b602918085fc38b8c0e68b1e4bc80bf1038fe
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-05-12 06:56:02 -0500

    Stubbed out test code.

--- InterviewCakeInCSharp/MakingChange/Program.cs
+++ InterviewCakeInCSharp/MakingChange/Program.cs
@@ -7,6 +7,25 @@ namespace MakingChange
     {
         public static void Main(string[] args)
         {
+            var combinations = GetCombinations(5, new []{10,2 , 3, 4});
+
+            foreach (var combination in combinations)
+            {
+                Console.WriteLine();
+                foreach (var denom in combination)
+                {
+                    Console.Write(Convert.ToString(denom) + " ");
+                }
+            }
+        }
+
+        public static List<int[]> GetCombinations(int money, int[] denominations)
+        {
+
+
+
+
+            return new List<int[]>();
         }
     }
 }
