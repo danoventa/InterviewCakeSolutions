8c04adfc021d0ce03a730eb82e401e85d3b9338f
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-05-22 13:39:58 -0500

    WIP split down, individual words now taken into account.'

--- InterviewCakeInCSharp/WordCloudData/Program.cs
+++ InterviewCakeInCSharp/WordCloudData/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace WordCloudData
 {
@@ -7,7 +8,44 @@ namespace WordCloudData
     {
         public static void Main(string[] args)
         {
+            var s = "This is how split work! If you'll take a look at dis, we can discuss it b'tter";
+            var l = Split(s);
+            foreach (var word in l)
+            {
+                Console.WriteLine(word);
+            }
+        }
 
+        public static List<string> Split(string inputString)
+        {
+            var returnList = new List<string>();
+            var waitForBlank = false;
+
+            var nextWord = "";
+            foreach (var c in inputString)
+            {
+                if (c == ' ' )
+                {
+                    if (waitForBlank)
+                    {
+                        waitForBlank = false;
+                    }
+                    returnList.Add(nextWord);
+                    nextWord = "";
+                    continue;
+                }
+                if (char.IsLetter(c))
+                {
+                    if (!waitForBlank)
+                    {
+                        nextWord += c.ToString();
+                    }
+                    continue;
+                }
+                waitForBlank = true;
+            }
+            returnList.Add(nextWord);
+            return returnList;
         }
     }
 }
