3e8b106c3e6d3ddffde2c9617c687a6feeb1f791
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-05-23 07:42:33 -0500

    Updated, with a second shuffle.

--- InterviewCakeInCSharp/InPlaceShuffle/Program.cs
+++ InterviewCakeInCSharp/InPlaceShuffle/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace InPlaceShuffle
@@ -11,11 +12,40 @@ namespace InPlaceShuffle
         {
 
             var arr = new [] {2, 3, 4, 5, 6};
+            Console.WriteLine("Original: " );
             foreach (var n in arr)
             {
-                Console.WriteLine("Original Val: " + n);
+                Console.Write(n + " " );
             }
+            
+            ShuffleInPlace(ref arr);
+            
+            Console.WriteLine("\n\nShuffled:");
+            foreach (var n in arr)
+            {
+                Console.Write(n + " " );
+            }
+            
+            var arr2 = new [] {2, 3, 4, 5, 6};
+            Console.WriteLine("\n\nOriginal 2: " );
+            foreach (var n in arr2)
+            {
+                Console.Write(n + " " );
+            }
+            
+            ShuffleInPlaceNotSame(ref arr2);
+            
+            Console.WriteLine("\n\nShuffled 2:");
+            foreach (var n in arr2)
+            {
+                Console.Write(n + " " );
+            }
+        }
+
+        public static void ShuffleInPlace(ref int[] arr)
+        {
             var rLoc = new Random(arr.Length);
+
             for (var i = 0; i < arr.Length; i++)
             {
                 var pos = rLoc.Next(0, arr.Length - 1);
@@ -23,9 +53,22 @@ namespace InPlaceShuffle
                 arr[i] = arr[pos];
                 arr[pos] = tempVal;
             }
-            foreach (var n in arr)
+        }
+        
+        public static void ShuffleInPlaceNotSame(ref int[] arr)
+        {
+            var rLoc = new Random(arr.Length);
+
+            for (var i = 0; i < arr.Length; i++)
             {
-                Console.WriteLine("Shuffled Val: " + n);
+                var pos = rLoc.Next(0, arr.Length - 1);
+                while (i == pos)
+                {
+                    pos = rLoc.Next(0, arr.Length - 1);
+                }
+                var tempVal = arr[i];
+                arr[i] = arr[pos];
+                arr[pos] = tempVal;
             }
         }
     }
