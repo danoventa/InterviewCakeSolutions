401881a863c19df7b65095528f8fa714c447b033
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-05-22 23:09:42 -0500

    ed thm.

---
+++ InterviewCakeInCSharp/InPlaceShuffle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+
+namespace InPlaceShuffle
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var arr = new [] {2, 3, 4, 5, 6};
+            var tempVal = 0;
+            var rLoc = new Random(arr.Length);
+            for (var i = 0; i < arr.Length; i++)
+            {
+                var pos = rLoc.Next(arr.Length);
+                tempVal = arr[i];
+                arr[i] = arr[tempVal];
+                arr[tempVal] = trmpVal;
+            }
+        }
+    }
+}
