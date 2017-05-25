eb183b9b79b7772f44b0befce37fa699067ed86d
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-05-25 06:07:05 -0500

    Missed a case with the breaking.

--- InterviewCakeInCSharp/StockPricesYesterday/Program.cs
+++ InterviewCakeInCSharp/StockPricesYesterday/Program.cs
@@ -9,21 +9,23 @@ namespace StockPricesYesterday
         public static void Main(string[] args)
         {
             var list = new List<int> {8, 8, 8, 8};
-            var stockPrices = new List<List<int>>();
-            stockPrices.Add(list);
-            stockPrices.Add(new List<int>
+            var stockPrices = new List<List<int>>
             {
-                1, 2, 3, 4, 5, 6, 7, 8
-            });
-            stockPrices.Add(new List<int>
-            {
-                8, 7 ,6, 5,4 ,3, 2 ,1
-            });
-            stockPrices.Add(new List<int>
-            {
-                1, 4 ,6, 2, 8, 9, 3, 5, 8, 2, 13, 5
-            });
-            
+                list,
+                new List<int>
+                {
+                    1,2,3,4,5,6,7,8
+                },
+                new List<int>
+                {
+                    8,7,6,5,4,3,2,1
+                },
+                new List<int>
+                {
+                    1,4,6,2,8,9,3,5,8,2,13,5
+                }
+            };
+
             foreach (var l in stockPrices)
             {
                 Console.WriteLine("Best you could do is: " + BestPossibleIncrease(l));
@@ -40,7 +42,18 @@ namespace StockPricesYesterday
             {
                 if (localWin == int.MinValue)
                 {
-                    localWin = 0;
+                    if (count - 1 >= 0)
+                    {
+                        localWin = stocks[count] - stocks[count - 1];
+                        if (localWin < 0)
+                        {
+                            localWin = 0;
+                        }
+                    }
+                    else
+                    {
+                        localWin = 0;
+                    }
                     topWin = Math.Max(localWin, topWin);
                     count++;
                     continue;
