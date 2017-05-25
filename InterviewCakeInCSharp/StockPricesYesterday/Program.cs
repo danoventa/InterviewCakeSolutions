daae949ae633771a8623fc0a88061e19a6f9b28b
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-05-24 23:11:47 -0500

    redoing in c# apple stock problem.

---
+++ InterviewCakeInCSharp/StockPricesYesterday/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StockPricesYesterday
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var stockPrices = new List<int> {6, 8, 43, 2, 1, 78, 9, 34, 23, 0, 3};
+            
+
+
+        }
+
+        public static int BestPossibleIncrease -=  
+       
+    }
+}
