c36d652a44a67a002e936f55ea9e8c228715ae38
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-05-25 06:14:15 -0500

    Stubbed next prblem.

---
+++ InterviewCakeInCSharp/ProductOfAllOtherNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductOfAllOtherNumbers
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        public static int ProductOfAllOtherNumebrs(List<int> lint)
+        {
+            return 1;
+        }
+    }
+}
