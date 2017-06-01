83b5679b0e83f33c1db38f71ea678a32d73830ee
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-05-31 22:45:04 -0500

    Dodging out.

--- InterviewCakeInCSharp/HIghestProductOf3/Program.cs
+++ InterviewCakeInCSharp/HIghestProductOf3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 namespace HIghestProductOf3
 {
@@ -7,6 +8,18 @@ namespace HIghestProductOf3
     {
         public static void Main(string[] args)
         {
+            var testCases = new List<List<int>>
+            {
+                new List<int> {1, 2, 3, 4, 5, 6, 7, 8}, // 336
+                new List<int> {9, 8, 7, 6, 5, 4, 3, 2}, // 504
+                new List<int> {-10, -5, 0, 2, 3, 5, 6}, // 300
+                new List<int> {-1, -2, -3, -4, -5} // -6
+            };
+            foreach(var test in testCases)
+            {
+                Console.WriteLine(HighestProductFromThree(test));
+            }
+
         }
 
         public static int HighestProductFromThree(List<int> arints)
@@ -15,8 +28,66 @@ namespace HIghestProductOf3
             {
                 throw new Exception("This function requires at least 3 integers in the List.");
             }
+
+            var max = int.MinValue;
+            var max2 = int.MinValue;
+            var max3 = int.MinValue;
+
+            var min = int.MaxValue;
+            var min2 = int.MaxValue;
+
+            foreach (var i in arints)
+            {
+                if (i <= min)
+                {
+                    min = i;
+                }
+                if (i > max)
+                {
+                    max = i;
+                }
+                
+                
+                if (max > int.MinValue && max2 < max * i)
+                {
+                    max2 = max * i;
+                }
+                else if (min < int.MaxValue && max2 < min * i)
+                {
+                    max2 = min * i;
+                }
+
+                if (max > int.MinValue && min2 > max * i)
+                {
+                    min2 = max * i;
+                } else if (min < int.MaxValue && min2 > min * i)
+                {
+                    min2 = max * i;
+                }
+
+                
+                if (min2 < int.MaxValue && max3 < min2 * i)
+                {
+                    max3 = min2 * i;
+                }
+                if (max2 > int.MinValue && max3 < max2 * i)
+                {
+                    max3 = max2 * i;
+                }
+                
+                
+                Console.WriteLine(max);
+                Console.WriteLine(max2);
+                Console.WriteLine(max3);
+                Console.WriteLine(min);
+                Console.WriteLine(min2);
+                Console.WriteLine();
+            }
             
-            return 1;
+            
+            
+            return max3;
         }
+        
     }
 }
