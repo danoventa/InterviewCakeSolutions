d5da5e32d8cca39e3073d9367133411114e61b1a
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-06-09 21:52:46 -0500

    New Make Change solved.

--- InterviewCakeInCSharp/MakingChange/Program.cs
+++ InterviewCakeInCSharp/MakingChange/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Security.Policy;
 
 namespace MakingChange
 {
@@ -8,12 +10,48 @@ namespace MakingChange
     {
         public static void Main(string[] args)
         {
-            var combinations = GetCombinations(5, new []{10,2 , 3, 4});
+            var combinations = GetCombinations(5, new []{1 ,2 , 3, 4});
+            var combos = DenomicationPermutation(new List<int> {1, 2, 3, 4}, 5);
 
-            Console.Write(Convert.ToString(combinations) + " ");
+            Console.Write("Combinations: " + Convert.ToString(combinations) + " ");
+            Console.Write("Combos " + Convert.ToString(combos) + " ");
         }
 
-        
+        public static int DenomicationPermutation(List<int> denominations, int target)
+        {
+            var availableNums = new List<int>();
+            foreach (var denomination in denominations)
+            {
+                var numsToUse = Math.Floor((double)(target/denomination));
+                for (var iter = 0; iter < numsToUse; iter++)
+                {
+                    availableNums.Add(denomination);
+                }
+            }
+
+            var compositionCount = 0;
+            for (var i = 1; i <= availableNums.Count; i++)
+            {
+                var trailAmount = target;
+                
+                for (var j = availableNums.Count - i; j > -1; j--)
+                {
+                    if (Math.Floor((double) trailAmount / availableNums[j]) > 0)
+                    {
+                        trailAmount -= availableNums[j];
+                        continue;
+                    }
+                    if (trailAmount > 0 && trailAmount < availableNums[j])
+                    {
+                        continue;
+                    }
+                    compositionCount++;
+                    break;
+                }
+            }
+            
+            return compositionCount;
+        }
         
         public static int GetCombinations(int money, int[] denominations)
         {
